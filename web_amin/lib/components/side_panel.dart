9098733f1e9b6155a9eb85007ae0e9740c8e02f7
Churchil-Oduor <churchilokechoduor@gmail.com>
2025-03-30 15:23:13 +0300

    Created the sidepanel component and incorporated it to main panel

---
+++ web_amin/lib/components/side_panel.dart
@@ -0,0 +1,111 @@
+import 'package:flutter/material.dart';
+import 'package:web_amin/components/custom_button.dart';
+
+class SidePanel extends StatefulWidget {
+  final List<String> items;
+  final Function(String) SelectedItem;
+
+  const SidePanel({super.key, required this.items, required this.SelectedItem});
+
+  @override
+  State<SidePanel> createState() => _SidePanelState();
+}
+
+class _SidePanelState extends State<SidePanel> {
+  int _selectedItem = -1;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.white,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          SizedBox(height: 20),
+          Text("Admin Panel", style: TextStyle(fontWeight: FontWeight.bold)),
+
+          Padding(
+            padding: EdgeInsets.only(left: 10, top: 20),
+            child: Row(
+              children: [
+                Image(
+                  image: AssetImage("assets/JKUATCU LOGO.png"),
+                  height: 70,
+                  width: 70,
+                ),
+                SizedBox(width: 10),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.end,
+
+                  children: [
+                    Text("JKUAT"),
+                    SizedBox(height: 1),
+                    Text("Christian Union"),
+                  ],
+                ),
+              ],
+            ),
+          ),
+
+          //space inbetween the Padding and the Text
+          SizedBox(height: 20),
+
+          Text("View & Manage", style: TextStyle(fontWeight: FontWeight.bold)),
+
+          SizedBox(height: 10),
+          Flexible(
+            child: ListView.builder(
+              itemCount: widget.items.length,
+              itemBuilder: (context, index) {
+                return ListTile(
+                  dense: true,
+                  title: Text(widget.items[index], style: TextStyle(
+                    fontWeight: _selectedItem == index ? FontWeight.bold : FontWeight.normal,
+                    
+                  )),
+                  onTap: () {
+                    setState(() {
+                      _selectedItem = index;
+                    });
+                    widget.SelectedItem(widget.items[index]);
+                  },
+                );
+              },
+            ),
+          ),
+
+          Align(
+            alignment: Alignment.bottomCenter,
+
+            child: SizedBox(
+              height: 50,
+              width: 230,
+
+              ///Nomination Button
+              child: CustomButton(
+                button_label: "Open Nominations",
+                color_grad_1: const Color.fromARGB(255, 245, 60, 47),
+                color_grad_2: const Color.fromARGB(255, 141, 12, 3),
+                onClick: () => {},
+              ),
+            ),
+          ),
+
+          ///Made by JKUATCU IT team text
+          Padding(
+            padding: EdgeInsets.only(left: 22, top: 20, bottom: 10),
+            child: Align(
+              alignment: Alignment.centerLeft,
+              child: Text(
+                "Made By JKUATCU IT",
+                style: TextStyle(fontSize: 10),
+                textAlign: TextAlign.start,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
--- web_amin/lib/pages/admin_panel.dart
+++ web_amin/lib/pages/admin_panel.dart
@@ -1,23 +1,28 @@
 import 'package:flutter/material.dart';
 import 'package:web_amin/components/custom_button.dart';
 import 'package:web_amin/components/custom_card.dart';
+import 'package:web_amin/components/side_panel.dart';
 
 class AdminPanel extends StatelessWidget {
   AdminPanel({super.key});
 
-  List items = [
-    ["Upcoming Tasks", Icon(Icons.task)],
-    ["Executive Committee"],
-    ["ETs & Ministries"],
-    ["Students & Associates"],
-    ["Upcoming Events", Icon(Icons.calendar_month)],
-    ["Summons", Icon(Icons.book)],
-    ["Suggestions", Icon(Icons.message_outlined)],
+  final List items = [
+    "Upcoming Tasks",
+    "Executive Committee",
+    "ETs & Ministries",
+    "Students & Associates",
+    "Upcoming Events",
+    "Summons",
+    "Suggestions",
   ];
 
   final double radius = 10;
   final double grid_spacing = 10;
   final double card_height = 130;
+
+  void tapped() {
+    print("I have been tapped");
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -29,110 +34,18 @@ class AdminPanel extends StatelessWidget {
         children: [
           Expanded(
             flex: 1,
-            child: Container(
-              color: Colors.white,
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: [
-                  SizedBox(height: 20),
-                  Text(
-                    "Admin Panel",
-                    style: TextStyle(fontWeight: FontWeight.bold),
-                  ),
-                  Padding(
-                    padding: EdgeInsets.only(left: 10, top: 20),
-                    child: Row(
-                      children: [
-                        Image(
-                          image: AssetImage("assets/JKUATCU LOGO.png"),
-                          height: 70,
-                          width: 70,
-                        ),
-                        SizedBox(width: 10),
-                        Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          mainAxisAlignment: MainAxisAlignment.end,
+            child: SidePanel(
+              items: [
+                "Upcoming Tasks",
+                "Executive Committee",
+                "ETs & Ministries",
+                "Accounts",
+                "Suggestions",
+              ],
 
-                          children: [
-                            Text("JKUAT"),
-                            SizedBox(height: 1),
-                            Text("Christian Union"),
-                          ],
-                        ),
-                      ],
-                    ),
-                  ),
-
-                  //space inbetween the Padding and the Text
-                  SizedBox(height: 20),
-
-                  Text(
-                    "View & Manage",
-                    style: TextStyle(fontWeight: FontWeight.bold),
-                    textAlign: TextAlign.start,
-                  ),
-
-                  SizedBox(height: 10),
-                  Flexible(
-                    child: ListView(
-                      children: [
-                        ListTile(
-                          leading: Text("Upcoming Activities"),
-                          trailing: Icon(
-                            Icons.task_outlined,
-                            color: const Color.fromARGB(255, 34, 31, 31),
-                          ),
-                          onTap: () {},
-                        ),
-                        ListTile(leading: Text("Executive Committee")),
-                        ListTile(leading: Text("ETs & Ministries")),
-                        ListTile(leading: Text("Students & Associates")),
-                        ListTile(
-                          leading: Text("Suggestion Box"),
-                          trailing: Icon(
-                            Icons.message_outlined,
-                            color: const Color.fromARGB(255, 34, 31, 31),
-                          ),
-                        ),
-
-                        SizedBox(height: 100),
-                      ],
-                    ),
-                  ),
-
-                  Align(
-                    alignment: Alignment.bottomCenter,
-
-                    child: SizedBox(
-                      height: 50,
-                      width: 230,
-
-                      ///Nomination Button
-                      child: CustomButton(
-                        button_label: "Open Nominations",
-                        color_grad_1: const Color.fromARGB(255, 245, 60, 47),
-                        color_grad_2:  const Color.fromARGB(255, 141, 12, 3),
-                        onClick: () => {},
-                    
-
-                      ),
-                    ),
-                  ),
-
-                  ///Made by JKUATCU IT team text
-                  Padding(
-                    padding: EdgeInsets.only(left: 22, top: 20, bottom: 10),
-                    child: Align(
-                      alignment: Alignment.centerLeft,
-                      child: Text(
-                        "Made By JKUATCU IT",
-                        style: TextStyle(fontSize: 10),
-                        textAlign: TextAlign.start,
-                      ),
-                    ),
-                  ),
-                ],
-              ),
+              SelectedItem: (selecteditem) {
+                print('$selecteditem');
+              },
             ),
           ),
 
